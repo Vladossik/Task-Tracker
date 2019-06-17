b9c64aa12b99d2d3fe835a99ba991a5c18d09e3e
vladislava <vlada-vakulenko@mail.ru>
2019-06-17 14:19:45 +0300

    Initial Commit

---
+++ Task tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task tracker
+//
+//  Created by Vladislava on 17/06/2019.
+//  Copyright © 2019 VladislavaVakulenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
