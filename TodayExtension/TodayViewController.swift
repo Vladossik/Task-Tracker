79e191f191723dbd55d11d4cd88a7398003da6f7
vladislava <vlada-vakulenko@mail.ru>
2019-06-25 12:47:46 +0300

    todayExtension

---
+++ TodayExtension/TodayViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TodayViewController.swift
+//  TodayExtension
+//
+//  Created by Vladislava on 23/06/2019.
+//  Copyright © 2019 VladislavaVakulenko. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+   
+    @IBOutlet weak var headTask: UILabel!
+    @IBOutlet weak var statusTask: UILabel!
+    @IBOutlet weak var dateTask: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+//        let myAppUrl = URL(string: "main-screen:")!
+//        extensionContext?.open(myAppUrl, completionHandler: { (success) in
+//            if (!success) {
+//                print("error: failed to open app from Today Extension")
+//            }
+//        })
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
